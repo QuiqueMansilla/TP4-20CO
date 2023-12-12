bf454df5c46db0636e92dbd074b5e1bb546cc5c8
QuiqueMansilla <quiquemansilla1@hotmail.com>
2023-12-11 21:26:09 -0300

    Se agrega documentación a las funciones que faltaban

--- inc/gpio.h
+++ inc/gpio.h
@@ -50,15 +50,36 @@ typedef struct gpio_h * gpio_t;
  * @brief Función para crear un puerto digital
  *
  * @param port Numero de puerto GPIO correspondiente
- * @return bit Numero de bit asignado al puerto
+ * @param bit Numero de bit asignado al puerto
  * @return gpio_t Puntero al objeto creado
  */
 gpio_t GpioCreate(uint8_t port, uint8_t bit);
 
+/**
+ * @brief Función para configurar la direccion del puerto digital como salida o como entrada
+ *
+ * @param gpio puntero al objeto obtenido al llamar a la función GpioCreate
+ * @param output configuración de salida del puerto digital: true => Salida, False => Entrada
+ * @return Void No devuelve valor de retorno
+ */
 void GpioSetDirection(gpio_t gpio, bool output);
 
+/**
+ * @brief Función para consultar la direccion del puerto digital, si es salida o entrada
+ *
+ * @param gpio puntero al objeto obtenido al llamar a la función GpioCreate
+ * @return true El puerto digital esta configurado como Salida
+ * @return false El puerto digital esta configurado como Entrada
+ */
 bool GpioGetDirection(gpio_t gpio);
 
+/**
+ * @brief Función para configurar el estado del puerto digital: On / Off
+ *
+ * @param gpio puntero al objeto obtenido al llamar a la función GpioCreate
+ * @param state El estado del puerto digital: true => Encender (On), false => Apagar (Off)
+ * @return Void No devuelve valor de retorno
+ */
 void GpioSetState(gpio_t gpio, bool state);
 
 /**
